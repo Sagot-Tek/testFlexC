5a1d765c54f39e1146600a55f8a3d593303ae574
Sagot-Tek <133714932+Sagot-Tek@users.noreply.github.com>
2025-04-09 21:12:38 +0000

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import fn_filmetrics
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 app.register_functions(fn_filmetrics.bp)
